547981137fbb074ef673507da0af93f931ba2183
Aman Shaikh <amanshaikh9156@gmail.com>
2026-04-13 01:47:40 +0530

    first commit

---
+++ src/main/java/com/icwd/hiber/App.java
@@ -0,0 +1,14 @@
+package com.icwd.hiber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "We are going to learn Hibernate" );
+    }
+}
